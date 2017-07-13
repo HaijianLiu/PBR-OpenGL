b7cda0dd9b025aa789dca0147d964a989d9a09bd
HaijianLiu <haijanliu@gmail.com>
2017-07-14 00:51:44 +0900

    shader is changing

--- PBR/Shader.hpp
+++ PBR/Shader.hpp
@@ -16,6 +16,77 @@
 #include <sstream>
 #include <iostream>
 
+#ifndef Shader_hpp
+#define Shader_hpp
+
+/*----------------------------------------------------------------------------*/
+class Shader {
+public:
+	unsigned int programID;
+	// constructor generates the shader on the fly
+	Shader(const char* vertexPath, const char* fragmentPath);
+	virtual ~Shader ();
+	// activate the shader
+	void use();
+	// utility uniform functions
+	void setBool(const char* name, bool value);
+	void setInt(const char* name, int value);
+	void setFloat(const char* name, float value);
+	void setVec2(const char* name, glm::vec2 value);
+	void setVec2(const char* name, float x, float y);
+	void setVec3(const char* name, glm::vec3 value);
+	void setVec3(const char* name, float x, float y, float z);
+	void setVec4(const char* name, glm::vec4 value);
+	void setVec4(const char* name, float x, float y, float z, float w);
+	void setMat2(const char* name, glm::mat2 mat);
+	void setMat3(const char* name, glm::mat3 mat);
+	void setMat4(const char* name, glm::mat4 mat);
+
+private:
+	unsigned int loadShader(std::string vertexPath, std::string fragmentPath);
+};
+/*------------------------------------------------------------------------------
+< Shader Class >
+
+Load vertexshader and fragmentshader
+Delete vertexshader and fragmentshader
+Create uniforms
+................................................................................
+Members:
+
+unsigned int programID
+................................................................................
+Example:
+
+Shader* shader = new Shader(vertexPath,fragmentPath);
+...
+delete shader;
+
+Shader shader = Shader(vertexPath,fragmentPath);
+------------------------------------------------------------------------------*/
+
+
+/*----------------------------------------------------------------------------*/
+// unsigned int loadShader(std::string vertexPath, std::string fragmentPath);
+/*------------------------------------------------------------------------------
+< Load Shaders >
+
+Load vertexshader and fragmentshader
+Create and compile GLSL program from the shaders
+................................................................................
+Return:
+
+unsigned int programID
+................................................................................
+Example:
+
+unsigned int programID = loadShader("vertexshader.glsl","fragmentshader.glsl");
+------------------------------------------------------------------------------*/
+
+
+#endif /* shaderloader_hpp */
+
+
 class Shader
 {
 public:

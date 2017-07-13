584f22e5599f92ab1f14c644acc3634024efa448
HaijianLiu <haijanliu@gmail.com>
2017-07-14 01:41:36 +0900

    Mesh

--- PBR/Mesh.hpp
+++ PBR/Mesh.hpp
@@ -2,45 +2,40 @@
 #define Mesh_hpp
 
 struct Vertex {
-    // position
-    glm::vec3 Position;
-    // normal
-    glm::vec3 Normal;
-    // texCoords
-    glm::vec2 TexCoords;
-    // tangent
-    glm::vec3 Tangent;
-    // bitangent
-    glm::vec3 Bitangent;
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 uv;
+	glm::vec3 tangent;
+	glm::vec3 bitangent;
 };
 
 struct Texture {
-    unsigned int id;
-    std::string type;
-    aiString path;
+	unsigned int id;
+	std::string type;
+	aiString path;
 };
 
 class Mesh {
 public:
-    /*  Mesh Data  */
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
-    std::vector<Texture> textures;
-    unsigned int VAO;
+	/*  Mesh Data  */
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+	unsigned int vertexArrayID;
 
-    /*  Functions  */
-    // constructor
-    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
-    // render the mesh
-    void Draw(Shader shader);
+	/*  Functions  */
+	// constructor
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+	// render the mesh
+	void Draw(Shader shader);
 
 private:
-    /*  Render data  */
-    unsigned int VBO, EBO;
+	/*  Render data  */
+	unsigned int vertexBuffer, elementBuffer;
 
-    /*  Functions    */
-    // initializes all the buffer objects/arrays
-    void setupMesh();
+	/*  Functions    */
+	// initializes all the buffer objects/arrays
+	void setupMesh();
 };
 
 #endif /* Mesh_hpp */
